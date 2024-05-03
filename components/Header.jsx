7bc36d25531d19890fe1f29dd4c21e6694dda642
Jorge Matos <jisapjisap@gmail.com>
2024-05-03 17:59:01 +0200

    header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import Logo from './Logo'
+import Nav from './Nav'
+import ThemeToggler from './ThemeToggler'
+import MobileNav from './MobileNav'
 
 const Header = () => {
+
+  const [header, setHeader] = useState(false);
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener('scroll', () => {
+      window.scrollY > 50
+        ? setHeader(true)
+        : setHeader(false)
+    });
+
+    return () => window.removeEventListener('scroll', scrollYPos)
+  })
+
   return (
-    <header>
+    <header className={`
+      ${header ? 'shadow-md !py-3 dark:!bg-secondary' : '' }
+      sticky top-0 z-30 transition-all max-padd-container flexBetween bg-[#fdf3fb] dark:bg:transparent py-5
+    `}>
+      
+      
       <Logo />
+      {/* nav & buttons */}
+      <div className='flexCenter gap-x-8'>
+        <Nav />
+        <ThemeToggler />
+        <div className='xl:hidden'>
+          <MobileNav />
+        </div>
+      </div>
     </header>
   )
 }
--- components/Logo.jsx
+++ components/Logo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Logo = () => {
   return (
     <Link href="/" className='bold-32 !font-extrabold'>
-      P<span className='text-primary'>folio</span>
+      P.<span className='text-primary'>folio</span>
     </Link>
   )
 }

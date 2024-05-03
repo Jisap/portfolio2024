70be273a87bc57e93281645b52e613d024ca9fd4
Jorge Matos <jisapjisap@gmail.com>
2024-05-03 09:44:05 +0200

    init

---
+++ app/contact/contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contactPage = () => {
+  return (
+    <div>Contact page</div>
+  )
+}
+
+export default contactPage
---
+++ app/portfolio/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const portfolioPage = () => {
+  return (
+    <div>Portfolio page</div>
+  )
+}
+
+export default portfolioPage

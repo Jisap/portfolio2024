31f54ceb13d8cce02c4f2fbd5382226d1aed4c64
Jorge Matos <jisapjisap@gmail.com>
2024-05-05 11:00:00 +0200

    about

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,7 @@
 import { Play } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import { Tabs, TabsList, TabsTrigger } from './ui/tabs'
 
 const About = () => {
   return (
@@ -24,11 +25,17 @@ const About = () => {
           <div className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flexCenter cursor-pointer'>
             <Play  className='h-16 w-16 bg-primary p-4 rounded-full z-20 to-primary-foreground'/>
           </div>
-          <div></div>
+          <div className='absolute h-11 bg-secondary rounded-full animate-ping z-10'></div>
         </div>
         {/* right */}
-        <div>
-          right side
+        <div className='flex mx-auto xl:mx-0'>
+          <Tabs defaultValue='intro'>
+            <TabsList>
+              <TabsTrigger value='intro'>Introduction</TabsTrigger>
+              <TabsTrigger value='education'>Education</TabsTrigger>
+              <TabsTrigger value='skills'>Skills</TabsTrigger>
+            </TabsList>
+          </Tabs>
         </div>
       </div>
     </section>

06e89daf67e33ef7215f1bd850b5bb82a1809491
Jorge Matos <jisapjisap@gmail.com>
2024-05-05 10:44:20 +0200

    about

---
+++ components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section>
+    {/* title */}
+      <div>
+        <span></span>
+      </div>
+    </section>
+  )
+}
+
+export default About

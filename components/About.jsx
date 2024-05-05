fc4a07778d406a80bca16f4dd0dab1b3c24965de
Jorge Matos <jisapjisap@gmail.com>
2024-05-05 10:50:05 +0200

    check-status

--- components/About.jsx
+++ components/About.jsx
@@ -1,11 +1,35 @@
+import { Play } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
   return (
-    <section>
+    <section className='max-padd-container py-16 xl:py-28'>
     {/* title */}
-      <div>
-        <span></span>
+      <div className='pb-10 text-center xl:text-start font-bold'>
+        <span className='text-primary uppercase'>MEET PARKER</span>
+        <h3 className='h3 font-extrabold'>About me</h3>
+      </div>
+      {/* container */}
+      <div className='flex flex-col xl:flex-row gap-16'>
+        {/* left */}
+        <div className='hidden xl:flexCenter flex-1 relative'>
+          <Image 
+            src={'/about.png'}
+            alt=''
+            height={444}
+            width={444}
+            className='rounded-lg'
+          />
+          <div>
+            <Play />
+          </div>
+          <div></div>
+        </div>
+        {/* right */}
+        <div>
+          right side
+        </div>
       </div>
     </section>
   )
